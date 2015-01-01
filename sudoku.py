57b0dd7d9715c805920d940d8229e02887cec1b2
Ciprian Cosma <cip.cosma@gmail.com>
2015-01-01 10:09:19 +0200

    sudoku solver

---
+++ sudoku.py
@@ -0,0 +1,8 @@
+'''
+Created on Jan 1, 2015
+
+@author: openstack
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ test/sudoku_tests.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 1, 2015
+
+@author: openstack
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
